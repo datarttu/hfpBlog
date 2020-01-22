1b1a32f38931125bcdea4b66b8630f2e1a5f55f6
Arttu Kosonen <36631098+datarttu@users.noreply.github.com>
2020-01-22 16:03:07 +0200

    Update raw_hfpv2_queries.sql

--- explore/raw_hfpv2_queries.sql
+++ explore/raw_hfpv2_queries.sql
@@ -1,6 +1,31 @@
 -- Using raw HFP v2 storage db
-SELECT event_type, (stop is null) AS stop_null, count(tst) 
-FROM bus 
+SELECT event_type, (stop is null) AS stop_null, count(tst)
+FROM bus
 WHERE event_type::text IN ('ARR', 'DEP', 'ARS', 'PDE', 'PAS', 'WAIT', 'DOO', 'DOC')
 GROUP BY 1, 2
 ORDER BY 1, 2;
+
+SELECT event_type, (stop is null) AS stop_null, count(tst)
+FROM tram
+WHERE event_type::text IN ('ARR', 'DEP', 'ARS', 'PDE', 'PAS', 'WAIT', 'DOO', 'DOC')
+GROUP BY 1, 2
+ORDER BY 1, 2;
+
+SELECT event_type, (stop is null) AS stop_null, count(tst)
+FROM train
+WHERE event_type::text IN ('ARR', 'DEP', 'ARS', 'PDE', 'PAS', 'WAIT', 'DOO', 'DOC')
+GROUP BY 1, 2
+ORDER BY 1, 2;
+-- All events are strictly related to a stop except 'DOO' and 'DOC'.
+
+SELECT event_type, (stop is null) AS stop_null, count(tst)
+FROM metro
+WHERE event_type::text IN ('ARR', 'DEP', 'ARS', 'PDE', 'PAS', 'WAIT', 'DOO', 'DOC')
+GROUP BY 1, 2
+ORDER BY 1, 2;
+-- No data returned for metro...
+SELECT event_type, count(tst)
+FROM metro
+GROUP BY event_type
+ORDER BY event_type;
+-- Metro only has VP events.
